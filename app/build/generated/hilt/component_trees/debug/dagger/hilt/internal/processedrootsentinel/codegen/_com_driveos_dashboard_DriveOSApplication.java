9f63c79f03a536276cafd92eba6109b598bb0769
Saithrishadaggupati <saithrishadaggupati@gmail.com>
2026-06-30 13:50:01 +0530

    Add .gitignore, remove build artifacts from tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_driveos_dashboard_DriveOSApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.driveos.dashboard.DriveOSApplication"
-)
-public final class _com_driveos_dashboard_DriveOSApplication {
-}
